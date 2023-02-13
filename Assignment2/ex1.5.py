bf03b687c680a9ec704d74641909a9694a763672
Huiying <huiying.zhenzhen@ucalgary.ca>
2023-02-12 22:35:38 -0700

    code for ex1.5

--- Assignment2/ex1.5.py
+++ Assignment2/ex1.5.py
@@ -16,32 +16,16 @@ def function1(n):
     else:
         return function1(n-1) + function1(n-2)
 
-if __name__ == "main":
-    n = 35
-    calc_f1 = function1(n)
-    calc_f2 = function2(n)
-    func1 = '''
-    def function1(n):
-        if n == 0 or n == 1:
-            return n
-        else:
-            return function1(n-1) + function1(n-2)'''
-
-    time_f1 = (timeit.timeit(stmt = func1,
-                     number = 1))
-    print(time_f1)
-
-    func2 = '''
-    def function2(n, prev_results = {}):
-        if n == 0 or n == 1:
-            return n
-        else:
-            if n in prev_results:
-                return prev_results[n]
-            else:
-                prev_results[n] = function2(n - 1) + function2(n - 2)
-                return prev_results[n]'''
+def main():
+    #original function
+    for i in range(36):
+        time1 = timeit.timeit(lambda: function1(i), number=1)
+        print("The time it takes to calculate the ",i, "Fibonacci number is:", time1, "seconds")
     
-    time_f2 = (timeit.timeit(stmt = func2,
-                     number = 1))
-    print(time_f2)
+    #function using memoization
+    for i in range(36):
+        time2 = timeit.timeit(lambda: function2(i), number=1)
+        print("The time it takes to calculate the ",i, "Fibonacci number is:", time2, "seconds")
+
+if __name__ == "main":
+    main()
